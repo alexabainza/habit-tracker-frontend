d10432ab001585d107034f3fd2a9a34e139777c9
Chuyaks123 <66161070+shetbricks21@users.noreply.github.com>
2024-11-24 11:09:53 +0800

    added a tooltip for the navbar routes in large screens

--- src/components/shared/navbar/Navigation.tsx
+++ src/components/shared/navbar/Navigation.tsx
@@ -24,23 +24,23 @@ const Navigation = () => {
     return (
         <ul className="font-medium space-y-5 z-50 shadow-sm">
             {MenuItems.map((item, index) => (
-                <TooltipProvider delayDuration={300}>
-                    <Tooltip>
-                        <TooltipTrigger>
-                            <li key={index}>
+                <li key={index}>
+                    <TooltipProvider delayDuration={300}>
+                        <Tooltip>
+                            <TooltipTrigger>
                                 <Link to={item.link} className={`border border-dashed rounded-md border-gray-600 flex-shrink-0 flex items-center gap-2 p-2.5 hover:rounded-2xl transition-all duration-300 ${location.pathname === item.link ? 'bg-blue-700 text-white' : 'hover:bg-gray-100'}`}>
                                     <item.icon className="h-6 w-6" />
                                     <span className="md:hidden">{item.name}</span>
                                 </Link>
-                            </li>
-                        </TooltipTrigger>
-                        <TooltipContent side="right" className="bg-black text-white text-md font-semibold flex items-center gap-x-1">
-                            To
-                            <p className="font-bold">{item.name}</p>
-                            <ChevronRightIcon className="h-4 w-4" />
-                        </TooltipContent>
-                    </Tooltip>
-                </TooltipProvider>
+                            </TooltipTrigger>
+                            <TooltipContent side="right" className="bg-black text-white text-md font-semibold flex items-center gap-x-1">
+                                To
+                                <p className="font-bold">{item.name}</p>
+                                <ChevronRightIcon className="h-4 w-4" />
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                </li>
             ))}
         </ul>
     )
