98b519582c492f9eb2cb1ac0bc0cc0c4a8157e82
Alex Abainza <abainza.alexandra@gmail.com>
2024-11-25 16:03:16 +0800

    chore: added percent habits accomplished for one day

--- src/pages/dashboard/Challenges.tsx
+++ src/pages/dashboard/Challenges.tsx
@@ -9,29 +9,35 @@ const ChallengeCard: React.FC<{
   habit: Habit["habit"];
   checked: boolean;
   onCheck: (id: string, checked: boolean) => void;
-}> = ({ habit, checked, onCheck }) => {
+  className: string;
+}> = ({ habit, checked, onCheck, className }) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const updateAccomplishedStatus = async () => {
       setLoading(true);
       try {
-        const response = await useFetch(`/habits/acc/${habit._id.toString()}`, "put");
+        const response = await useFetch(
+          `/habits/acc/${habit._id.toString()}`,
+          "put"
+        );
         if (response.status === 204) {
           toast.error("The data does not exist!");
           return;
         }
       } catch (error) {
-        toast.error("Did not update habit. Reverting...")
+        toast.error("Did not update habit. Reverting...");
       } finally {
         setLoading(false);
       }
-    }
+    };
     updateAccomplishedStatus();
-  }, [checked])
+  }, [checked]);
 
   return (
-    <div className="flex flex-1 bg-white border py-4 border-gray-300 rounded-2xl px-8 gap-4 align-middle justify-between">
+    <div
+      className={`flex flex-1 bg-white border py-4 border-gray-300 rounded-2xl px-8 gap-4 align-middle justify-between transform transition-transform duration-500 ease-in-out ${className}`}
+    >
       <div className="flex gap-4 items-center">
         <Dumbbell />
         <span className="text-xl font-medium">{habit.name}</span>
@@ -51,8 +57,9 @@ const ChallengeCard: React.FC<{
             />
             <label
               htmlFor={habit._id}
-              className={`cursor-pointer w-6 h-6 rounded-lg ${checked ? "bg-sageGreen" : "bg-gray-200"
-                } flex items-center justify-center`}
+              className={`cursor-pointer w-6 h-6 rounded-lg ${
+                checked ? "bg-sageGreen" : "bg-gray-200"
+              } flex items-center justify-center`}
             >
               {checked && <span className="text-white font-bold">✔</span>}
             </label>
